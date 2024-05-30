5a62b161daedd17bd4096a88242da18b78cc7e1f
Nilesh <nilesh.k@m8itsolutions.com>
2024-05-30 19:39:59 +0530

    Initial Commit

---
+++ gmanlabsDemoProjects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gmanlabsDemoProjects
+//
+//  Created by m8nilesh on 30/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
